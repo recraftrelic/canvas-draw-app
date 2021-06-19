d933a5241b627ff8a1d6b966501e3442a11200ae
Manoj Singh Negi <justanothermanoj@gmail.com>
2021-06-19 11:42:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+const colors = [
+  "red",
+  "green",
+  "yellow",
+  "black",
+  "blue"
+]
 
 function App() {
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
+
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [lastPosition, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      ctx.current = canvasRef.current.getContext('2d');
+    }
+  }, []);
+
+  const draw = useCallback((x, y) => {
+    if (mouseDown) {
+      ctx.current.beginPath();
+      ctx.current.strokeStyle = selectedColor;
+      ctx.current.lineWidth = 10;
+      ctx.current.lineJoin = 'round';
+      ctx.current.moveTo(lastPosition.x, lastPosition.y);
+      ctx.current.lineTo(x, y);
+      ctx.current.closePath();
+      ctx.current.stroke();
+
+      setPosition({
+        x,
+        y
+      })
+    }
+  }, [lastPosition, mouseDown, selectedColor, setPosition])
+
+  const download = async () => {
+    const image = canvasRef.current.toDataURL('image/png');
+    const blob = await (await fetch(image)).blob();
+    const blobURL = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = blobURL;
+    link.download = "image.png";
+    link.click();
+  }
+
+  const clear = () => {
+    ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
+  }
+
+  const onMouseDown = (e) => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY
+    })
+    setMouseDown(true)
+  }
+
+  const onMouseUp = (e) => {
+    setMouseDown(false)
+  }
+
+  const onMouseMove = (e) => {
+    draw(e.pageX, e.pageY)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        style={{
+          border: "1px solid #000"
+        }}
+        width={400}
+        height={400}
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseUp}
+        onMouseMove={onMouseMove}
+      />
+      <br />
+      <select
+        value={selectedColor}
+        onChange={(e) => setSelectedColor(e.target.value)}
+      >
+        {
+          colors.map(
+            color => <option key={color} value={color}>{color}</option>
+          )
+        }
+      </select>
+      <button onClick={clear}>Clear</button>
+      <button onClick={download}>Download</button>
     </div>
   );
 }
